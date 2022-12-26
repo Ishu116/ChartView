bb43b82f0766c0a22eaddda7b158aea59dc62516
Ishu116 <yashpratapr@gmail.com>
2022-12-26 12:37:51 +0530

    passportJS added in authentication

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const path = require("path");
 const https = require("https");
 const mongoose = require("mongoose");
 // const md5 = require("md5");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const session = require("express-session")
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const { urlencoded } = require("body-parser");
 const { log } = require("console");
 
@@ -22,21 +25,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'Logos')));
 app.use(express.static(path.join(__dirname, 'background')));
 
-mongoose.connect(`mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.5nu8yr2.mongodb.net/userDB`, { useNewUrlParser: true });
+mongoose.set("strictQuery", false);
+
+app.use(session({
+    secret: "Out little Secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// mongoose.connect(`mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.5nu8yr2.mongodb.net/userDB`, { useNewUrlParser: true });
+
+mongoose.connect("mongodb://0.0.0.0:27017/userDB", {useNewUrlParser: true}, process.env.MONGO_URL);
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    username: String,
     email: String,
     password: String
 });
 
-const secret = process.env.SECRET;
+// const secret = process.env.SECRET;
 
 // console.log(process.env.SECRET);
 
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
+
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 let logName = false;
 let username = "";
@@ -45,6 +68,14 @@ app.get("/", function (req, res) {
     // console.log(process.env.SECRET);
     res.render("content", { label: logName ? username : "Get Started" });
 });
+
+app.get("/content",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("content", { label: logName ? username : "Get Started" });
+    }else{
+        res.redirect("/login");
+    }
+})
 
 app.get("/contact", function (req, res) {
     res.render("contact", { label: logName ? username : "Get Started" });
@@ -112,51 +143,36 @@ app.get("/chart", function (req, res) {
 });
 
 
-app.post("/signup", (req, res) => {
-    const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+app.post("/signup", function(req,res){
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+     if(err){
+         console.log(err);
+         res.redirect("/signup");
+     }
+     else {
+         passport.authenticate("local")(req,res,function(){
+             res.redirect("/content");
+         });
+     }
     });
+ });
 
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            logName = true;
-            username = req.body.name;
-            res.render("content", { label: logName ? username : "Get Started" });
-        }
+ app.post("/login", function(req,res){
+    const user = new User({
+     username: req.body.username,
+     password: req.body.password
     });
-});
-
-app.post("/login", (req, res) => {
-    const name = req.body.name;
-    const username = req.body.email;
-    const password = req.body.password;
-
-    User.findOne({ email: username }, (err, foundUser) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    logName = true;
-                    username = req.body.name;
-                    res.render("content", { label: logName ? username : "Get Started", headName: logName });
-                }
-                else {
-                    res.render("errorpage");
-                }
-            }
-            else {
-                res.render("errorpage");
-            }
-        }
+ 
+    req.login(user, function(err){
+     if(err){
+        console.log(err);
+     }else{
+         passport.authenticate("local")(req,res,function(){
+             res.redirect("/content");
+         });
+     }
     });
-});
+ });
 
 app.post("/", function (req, res) {
     res.redirect("/");
